b10275e39a2cadf1b526240fd2b2a20d56c80f6a
iñigo ezcurdia <inigoezcurdia@gmail.com>
2016-02-24 18:54:46 +0100

    Añadidos comentarios explicativos y carpeta de libreriras con ruta relativa

--- RSNG1516-P1S/src/rsng1516/p1s/s_server.java
+++ RSNG1516-P1S/src/rsng1516/p1s/s_server.java
@@ -1,14 +1,23 @@
-
+/**
+ * Se pretende construir un sistema de anuncio de videos bajo demanda o canales de streaming para una red IPv6.
+ * Repositorio: https://github.com/ChristopherCastro/RSNG1516-P1S
+ */
 package rsng1516.p1s;
 
 /**
  *
+ * @author Carolina Barcelo
  * @author Christpher Castro
  * @author Iñigo Ezcurdia
  */
 public class s_server {
 
     /**
+     * s_server -f conffile -p puerto -m mdir -o mpuerto
+     * conffile: fichero de programas a servir
+     * puerto: puerto en el que el servidor escuchará peticiones de clientes
+     * mdir: dirección a la que el servidor enviará los anuncios
+     * mpuerto: puerto al que el servidor enviará los anuncios
      * @param args the command line arguments
      */
     public static void main(String[] args) {
