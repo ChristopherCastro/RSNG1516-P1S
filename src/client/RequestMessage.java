fb197a45dddaeda6c50e00b426b7e85655adf668
Carolina Barcelo <carolinabarcelooliver@hotmail.com>
2016-03-02 19:30:58 +0100

    refact

--- src/client/RequestMessage.java
+++ src/client/RequestMessage.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package client;
 
-/**
- *
- * @author Carol
- */
+
 public class RequestMessage {
     
     protected String req;
@@ -23,6 +16,7 @@ public class RequestMessage {
         this.port = port;
         this.clientAdress = clientAdress;
     }
+    
     
     
     
