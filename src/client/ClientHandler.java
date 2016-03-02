1686f17463d58f5017afe59d5985309bd231a6ca
Carolina Barcelo <carolinabarcelooliver@hotmail.com>
2016-03-02 18:27:47 +0100

    void clientHandler

---
+++ src/client/ClientHandler.java
@@ -0,0 +1,16 @@
+package client;
+
+import java.io.File;
+
+public class ClientHandler implements Runnable{
+  
+    
+    public ClientHandler(String config, int puerto) {        
+    }
+    
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
