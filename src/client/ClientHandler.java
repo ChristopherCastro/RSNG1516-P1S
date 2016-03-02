ee0c133af9e32610989c56abee42065ea2f5e37a
Chris <chris@quickapps.es>
2016-03-02 18:41:29 +0100

    ClientHandler

--- src/client/ClientHandler.java
+++ src/client/ClientHandler.java
@@ -1,17 +1,34 @@
 package client;
 
-import java.io.File;
+import channel.ChannelCollection;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import main.Configuration;
 
-public class ClientHandler implements Runnable{
-  
-    
-    public ClientHandler(Socket s) {        
+public class ClientHandler implements Runnable {
+
+    protected InputStream in = null;
+    protected OutputStream out = null;
+    protected ChannelCollection channels;
+
+    public ClientHandler(Socket s, ChannelCollection channels) {
+        try {
+            this.in = s.getInputStream();
+            this.out = s.getOutputStream();
+            this.channels = channels;
+        } catch (IOException ex) {
+            Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        (new Thread(this)).start();
     }
     
     @Override
     public void run() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
 }
