5349313db98864f6e97813a0ab839b44e65e7655
Chris <chris@quickapps.es>
2016-03-02 18:20:24 +0100

    refactor

--- src/channel/Channel.java
+++ src/channel/Channel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rsng1516.p1s;
+package channel;
 
 public class Channel {
     
--- src/channel/ChannelCollection.java
+++ src/channel/ChannelCollection.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package rsng1516.p1s;
+package channel;
 
+import channel.Channel;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
