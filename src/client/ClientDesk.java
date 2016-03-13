e147fca3e0693378a7508572e6db7b13c93d0450
QuickApps <info@quickapps.es>
2016-03-13 15:06:46 +0100

    added user connected msg

--- src/client/ClientDesk.java
+++ src/client/ClientDesk.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import main.Configuration;
+import tools.IpTools;
 
 /**
  * Se encarga de escuchar en un puerto determinado a la espera de peticiones por parte de los clientes.
@@ -31,12 +33,13 @@ public class ClientDesk implements Runnable {
                     config.getServerPort()
                 )
             );
-            
+
             // WAIT FOR CLIENTS
             System.out.println("[ClientDesk] Waiting for clients");
             while (true) {
-                new ClientHandler(serverSocket.accept(), this.config.getChannelCollection());
-                System.out.println("[ClientDesk] New client connected");
+                Socket clientSocket = serverSocket.accept();
+                System.out.printf("[ClientDesk] New client connected from %s", IpTools.clientAddress(clientSocket));
+                new ClientHandler(clientSocket, this.config.getChannelCollection());
             }
         } catch (IOException ex) {
             Logger.getLogger(ClientDesk.class.getName()).log(Level.SEVERE, null, ex);
